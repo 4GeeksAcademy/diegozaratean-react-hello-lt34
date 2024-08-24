d330add579090cdd3dd99f5f7cf0cfe976384d1e
diego <diegozaratean@gmail.com>
2024-08-24 02:01:33 +0000

    react comopnent class

---
+++ src/js/component/boton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Boton = ()=>{
+    return(
+        <button className="btn btn-primary px-5 mb-5" type="button">
+            compra la secuela
+        </button>
+    )
+}
+
+export default Boton
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 // 4. Importar componente
 import Nav from "./nav"; 
+import Jumbotron from "./jumbotron";
+import Boton from "./boton";
 // 5. Usar componente
 
 
@@ -9,8 +11,10 @@ const Home = () => {
 	return (
 		<div className="text-center">
 			<Nav/>
+			<Jumbotron />
 
 			<h1 className="text-center mt-5">Bienvenidos a React ;) 123</h1>
+			<Boton />
 		</div>
 	);
 };
